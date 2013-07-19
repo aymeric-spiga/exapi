bb1f3ed5b27f52cba94737f53728cb069b88e0cf
Aymeric Spiga <aymeric.spiga@upmc.fr>
2013-07-19 08:39:13 +0000

    UTIL PYTHON. added a nice script by Tanguy able to plot histograms.
    git-svn-id: http://svn.lmd.jussieu.fr/Planeto/trunk/UTIL/PYTHON/api@1008 ca07c32c-8b1f-4cef-a7c7-6a85da90e321

--- exapi.py
+++ exapi.py
@@ -27,7 +27,7 @@ parser.print_version()
 
 # define options and get options+arguments
 # ----------------------------------------
-parser.add_option('-v','--var',action='store',dest='var',type="string",default=None,help='Variables processed. Not used means all variables.')
+parser.add_option('-v','--var',action='store',dest='var',type="string",default=None,help='Variables processed. Not used means all variables. tk,tpot,uvmet can be requested.')
 parser.add_option('-i','--interp',action='store',dest='itp',type="int",default=defitp,help='Mode: 2=press /  3=z-amr / 4=z-als (default)')
 parser.add_option('-l','--level',action='store',dest='lvl',type="string",default=deflvl,help='Levels: start[,stop,step] (-i 2: Pa)(-i 3,4: km)')
 parser.add_option('-o','--output',action='store',dest='output',type="string",default=None,help="name of output files")
