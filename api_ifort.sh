0b9042869ba69622c2a061c46894e92c5a3a9e40
Aymeric Spiga <aymeric.spiga@upmc.fr>
2015-02-05 14:57:07 +0100

    cmmented a useless line

--- api_ifort.sh
+++ api_ifort.sh
@@ -8,4 +8,4 @@ touch loge
 \rm logc
 \rm loge
 f2py -c -m api        api.F90   --fcompiler=intelem  -L$NETCDF/lib -lnetcdf -lm -I$NETCDF/include >  logc 2>  loge
-f2py -c -m timestuff  time.F    --fcompiler=intelem                                               >> logc 2>> loge
+#f2py -c -m timestuff  time.F    --fcompiler=intelem                                               >> logc 2>> loge
